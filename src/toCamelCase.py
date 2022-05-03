e8c1f0712d3d3f33a72a9901b86689b5e5b600c7
Maxwell Wang <maxwang@udel.edu>
2022-05-03 13:54:50 -0400

    camel case

--- src/toCamelCase.py
+++ src/toCamelCase.py
@@ -1,3 +1,9 @@
+from operator import sub
+
+
 def to_camel_case(text):
-    dashes = text.split("-").split("_")
-    return ""
+    #Check to see if there are are - or _ shown then subsitute those out, then adds to the text
+    #Also looks at the title
+    together = sub(r"(_|-)+", " " , text).title().replace(" ", "")
+    #Lowercases first letter of word and no spaces then joins the rest of the letters
+    return ''.join([together[0].lower(), together[1:]])
