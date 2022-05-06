dab49e98b32c67fa71c4d21014acaae72c7726ea
Maxwell Wang <maxwang@udel.edu>
2022-05-06 11:55:18 -0400

    looped over the text chars

--- src/toCamelCase.py
+++ src/toCamelCase.py
@@ -1,9 +1,16 @@
-from operator import sub
-
-
 def to_camel_case(text):
+    #Checks if text length is zero
+    if(text.length == 0):
+        return 
     #Check to see if there are are - or _ shown then subsitute those out, then adds to the text
     #Also looks at the title
-    together = sub(r"(_|-)+", " " , text).title().replace(" ", "")
+    #Joins the first char by uppercasing
+    together = ''.join(text[0].upper())
+    for i in range(1, len(text)):
+        if(text[i] == ' '):
+            together = together[i+1].upper()
+            i+=1
+        elif(text[i - 1] != ' '):
+            together+=together[i]
     #Lowercases first letter of word and no spaces then joins the rest of the letters
-    return ''.join([together[0].lower(), together[1:]])
+    return together
