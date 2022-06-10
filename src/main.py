70b1803c637791abb89e30bb372884ed14f323b7
mwang840 <maxwang@udel.edu>
2022-06-09 21:45:44 -0400

    involved sleep in function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+from codingBat.sleep_in import sleep_in
 from toCamelCase import *
 def main():
     camel = "Camel"
     print(to_camel_case(camel))
+    print(sleep_in(True, True))
     
 if __name__ == "__main__":
     main()    
