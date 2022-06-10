bdaf0e14fb9f79e7fc603cb735810cd8b8ff88ee
mwang840 <maxwang@udel.edu>
2022-06-09 21:45:34 -0400

    reduced if statements and improved while loop

--- src/romanNumeralEncoder.py
+++ src/romanNumeralEncoder.py
@@ -1,23 +1,26 @@
-
+import math
 def romanEncoder(num):
-    romans = {1:"I", 5: "V", 10: "X", 50: "L", 100: "C", 500:"D", 1000: "M"}
+    romans = {1:"I", 5: "V", 10: "X", 50: "L", 100: "C", 500:"D", 1000: "M", 5000: "G",
+            10000: "H"}
     oddRoman = {4:"IV", 9: "IX", 40: "XL", 90: "XC", 400: "CD", 900: "CM"}
     asRoma = ""
-    while(num > 0):
-        if(num % 1000 == 0):
-            asRoma += romans[1000]
-        elif(num % 500 == 0):
-            asRoma += romans[5]
-        elif(num % 100 == 0):
-            asRoma += romans[100]
-        elif(num % 50 == 0):
-            asRoma += romans[50]
-        elif(num % 10 == 0):
-            asRoma += romans[10]
-        elif(num % 5 == 0):
-            asRoma += romans[5]
-        else:
-            asRoma += romans[1]            
-        num = num / 10
+    dividend = 1
+    while (num > dividend):
+        dividend*=10
 
+    dividend /= 10
+
+    while(dividend):
+        trueDivide = int(num / dividend)
+        if(trueDivide <= 3):
+            asRoma += romans[dividend] * trueDivide
+        elif(trueDivide == 4):
+            asRoma += romans[dividend] + romans[dividend * 5]    
+        elif(5 <= trueDivide <= 8):
+            asRoma += romans[dividend * 5] + (romans[dividend] + trueDivide - 5)
+        elif(trueDivide == 9):
+            asRoma+= romans[dividend] + (romans[dividend * 10])
+        num = math.floor(num % dividend)
+        dividend /= 10    
+                
     return asRoma        
