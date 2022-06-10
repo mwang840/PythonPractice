24076a74e3de374eae86ef4cda7c2eacdc0359cf
mwang840 <maxwang@udel.edu>
2022-06-10 10:16:17 -0400

    implemented is prime function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from codingBat.sleep_in import sleep_in
+from isPrime import is_prime
 from toCamelCase import *
 def main():
     camel = "Camel"
     print(to_camel_case(camel))
     print(sleep_in(True, True))
+    print(is_prime(3))
     
 if __name__ == "__main__":
     main()    
