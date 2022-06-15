0bc676c3308f8cca1aca3296f792c804f7b14205
mwang840 <maxwang@udel.edu>
2022-06-14 20:47:27 -0400

    added more tests in main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from codingBat.sleep_in import sleep_in
+from countAllChar import count
 from isPrime import is_prime
-from toCamelCase import *
 def main():
-    camel = "Camel"
-    print(to_camel_case(camel))
     print(sleep_in(True, True))
     print(is_prime(3))
+    print(count("Houston"))
     
 if __name__ == "__main__":
     main()    
