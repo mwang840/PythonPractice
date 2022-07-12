5ef3c90a30791cc05eb26248d935beae83b318d9
mwang840 <maxwang@udel.edu>
2022-07-11 21:08:04 -0400

    0%

--- src/zerosToTheEnd.py
+++ src/zerosToTheEnd.py
@@ -1,2 +1,24 @@
 def move_zeros(lis):
-    return lis
+    endZeros = []
+    nonZeros = []
+    for li in range(len(lis)):
+        if lis[li] == 0:
+            endZeros.append(lis[li])
+        else:
+            nonZeros.append(lis[li])               
+
+    for end in endZeros:
+        nonZeros.append(endZeros[end])
+        
+
+    return nonZeros
+
+def main():
+    print(move_zeros([1, 0, 1, 2, 0, 1, 3]))
+    print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
+    print(move_zeros([0,0]))
+    print(move_zeros([]))
+    return    
+
+if __name__ == "__main__":
+    main()        
