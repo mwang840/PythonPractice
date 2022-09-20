3691a165d499ed30a8927b2c27327cc027506e02
mwang840 <maxwang@udel.edu>
2022-09-20 19:40:29 -0400

    dead fish mate

--- src/DeadFish.py
+++ src/DeadFish.py
@@ -1,4 +1,21 @@
 def parse(data):
     parsed = []
-    
-    return []
+    score = 0
+    for d in data:
+        if d == "i":
+            score += 1
+        elif d == 'd':
+            score -= 1
+        elif d == 's':
+            score *= score    
+        elif d == 'o':
+            parsed.append(score)        
+        
+    return parsed
+
+def main():
+    parsing = "isoisoiso"
+    print(parse(parsing))
+
+if __name__ == "__main__":
+    main()        
